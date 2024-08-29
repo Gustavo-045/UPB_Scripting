7fe4787f684c802f0e26ebc0a064d2ba067b3ce9
Gustavo Lora <gustavo.lora@upb.edu.co>
2024-08-28 21:21:38 -0500

    COMMIT 3

---
+++ Assets/Proyecto/Scripts/ejemploEvento.cs
@@ -0,0 +1,46 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using MoreMountains.CorgiEngine;
+using MoreMountains.Tools;
+
+public class ejemploEvento : MonoBehaviour, MMEventListener<PickableItemEvent>
+{
+
+    private int contador_monedas;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+        void OnEnable()
+    {
+        this.MMEventStartListening<PickableItemEvent>();
+    }
+    void OnDisable()
+    {
+        this.MMEventStopListening<PickableItemEvent>();
+    }
+
+    public virtual void OnMMEvent(PickableItemEvent e)
+    {
+        if(contador_monedas>=10)
+        {
+            GameManager.Instance.AddPoints(100);
+            GameManager.Instance.GainLives(2);
+            contador_monedas = 0;
+        }
+        else
+        {
+            contador_monedas++;
+        }
+       
+    }
+}
