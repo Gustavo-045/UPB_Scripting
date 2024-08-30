08cd790059a19b5b7070fe35040c26583e4cfee5
Gustao Lora <gustavo.lora@upb.edu.co>
2024-08-30 03:41:27 -0500

    Added a Game Event
    finaly jesus lord

---
+++ Assets/Proyecto/Scripts/IncrementarSalud.cs
@@ -0,0 +1,43 @@
+using MoreMountains.CorgiEngine;
+using MoreMountains.Tools;
+using System.Collections;
+using UnityEngine;
+
+public class IncrementarSalud : MonoBehaviour, MMEventListener<PickableItemEvent>
+{
+    private Health salud_personaje;
+
+    void OnEnable()
+    {
+        this.MMEventStartListening<PickableItemEvent>();
+    }
+
+    void OnDisable()
+    {
+        this.MMEventStopListening<PickableItemEvent>();
+    }
+
+    public virtual void OnMMEvent(PickableItemEvent e)
+    {
+        if (e.PickedItem.name == "Estrella")
+        {
+            IncrementarSaludJugador();
+        }
+    }
+
+    public Health GetHealthComponent()
+    {
+        GameObject player = LevelManager.Instance.Players[0].gameObject;
+        return player.GetComponent<Health>();
+    }
+
+    public void IncrementarSaludJugador()
+    {
+        salud_personaje = GetHealthComponent();
+        if (salud_personaje != null)
+        {
+            // Llamar al método GetHealth para incrementar la salud en 2 puntos
+            salud_personaje.GetHealth(2f, gameObject); 
+        }
+    }
+}
