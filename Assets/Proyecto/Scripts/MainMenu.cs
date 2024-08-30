58ab830761a7aadb7e44ef807d523ace66c11f84
Gustavo Lora <gustavo.lora@upb.edu.co>
2024-08-30 05:00:49 -0500

    Added Instructions Screen

--- Assets/Proyecto/Scripts/MainMenu.cs
+++ Assets/Proyecto/Scripts/MainMenu.cs
@@ -5,12 +5,24 @@ using UnityEngine.SceneManagement;
 public class MainMenu : MonoBehaviour
 {
 
-    
+    public GameObject PanelInstrucciones;
+
     // Start is called before the first frame update
     public void OnButton()
     {
         SceneManager.LoadScene("level 1");
     }
+
+      public void BackToMenu()
+    {
+        PanelInstrucciones.SetActive(false);
+    }
+
+        public void LoadInstructions()
+    {
+        PanelInstrucciones.SetActive(true);
+    }
+
 
     // Update is called once per frame
     void Update()
